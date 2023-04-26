206f8a8130a5052dd7fbb5bb53a92c3aa88c24a5
JHONATA_MOREIRA <jhonata.filizola@gmail.com>
2023-04-25 23:01:38 -0300

    Adicionado o novo projeto de pagamento

---
+++ hr-payholl/src/test/java/com/devsuperior/hrpayholl/HrPayhollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrpayholl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayhollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
