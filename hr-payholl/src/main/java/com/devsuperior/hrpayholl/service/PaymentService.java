949e9568a27315638073007ea33b4abe2e06ff54
JHONATA_MOREIRA <jhonata.filizola@gmail.com>
2023-05-03 10:08:26 -0300

    Implementa o FeignClient para substituir o RestTemplate

--- hr-payholl/src/main/java/com/devsuperior/hrpayholl/service/PaymentService.java
+++ hr-payholl/src/main/java/com/devsuperior/hrpayholl/service/PaymentService.java
@@ -1,13 +1,21 @@
 package com.devsuperior.hrpayholl.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.hrpayholl.entities.Payment;
+import com.devsuperior.hrpayholl.entities.Worker;
+import com.devsuperior.hrpayholl.feignclient.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
+	
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Jhonata", 200.0, days);
+		
+		Worker worker = workerFeignClient.findById(workerId).getBody();
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
